098e5189999c4e51d44472d28ba1ad8232a3ee6c
Vincent Germain <vgermain@scaleway.com>
2022-10-24 08:56:33 +0200

    fix(tem): remove duplicate code (#170)

---
+++ packages/clients/src/api/tem/v1alpha1/index.ts
@@ -0,0 +1,2 @@
+export { TemV1Alpha1GenAPI as API } from './api.gen'
+export * from './types.gen'
--- packages/clients/src/api/transactional_email/v1alpha1/index.ts
+++
@@ -1,2 +0,0 @@
-export { TransactionalEmailV1Alpha1GenAPI as API } from './api.gen'
-export * from './types.gen'
