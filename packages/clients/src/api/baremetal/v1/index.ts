337b16407cd9fa3661f86d23625272bba868cd9d
Vincent Germain <vgermain@scaleway.com>
2022-10-26 14:12:03 +0200

    Revert "feat(baremetal): add private network API (#169)"
    This reverts commit 03f17938dc24c12bbd063d436ae5335cf6c4f38b.

--- packages/clients/src/api/baremetal/v1/index.ts
+++ packages/clients/src/api/baremetal/v1/index.ts
@@ -1,6 +1,3 @@
-export { 
-  BaremetalPrivateNetworkV1GenAPI as PrivateNetworkAPI,
-  BaremetalV1UtilsAPI as API
-} from './api.utils'
+export { BaremetalV1UtilsAPI as API } from './api.utils'
 export * from './content.gen'
 export * from './types.gen'
--- packages/clients/src/api/baremetal/v1/marshalling.gen.ts
+++ packages/clients/src/api/baremetal/v1/marshalling.gen.ts
@@ -23,7 +23,6 @@ import type {
   ListOffersResponse,
   ListOptionsResponse,
   ListServerEventsResponse,
-  ListServerPrivateNetworksResponse,
   ListServersResponse,
   ListSettingsResponse,
   Memory,
@@ -33,17 +32,13 @@ import type {
   OfferOptionOffer,
   Option,
   PersistentMemory,
-  PrivateNetworkApiAddServerPrivateNetworkRequest,
-  PrivateNetworkApiSetServerPrivateNetworksRequest,
   RaidController,
   RebootServerRequest,
   Server,
   ServerEvent,
   ServerInstall,
   ServerOption,
-  ServerPrivateNetwork,
   ServerRescueServer,
-  SetServerPrivateNetworksResponse,
   Setting,
   StartBMCAccessRequest,
   StartServerRequest,
@@ -331,25 +326,6 @@ const unmarshalServerEvent = (data: unknown) => {
   } as ServerEvent
 }
 
-export const unmarshalServerPrivateNetwork = (data: unknown) => {
-  if (!isJSONObject(data)) {
-    throw new TypeError(
-      `Unmarshalling the type 'ServerPrivateNetwork' failed as data isn't a dictionary.`,
-    )
-  }
-
-  return {
-    createdAt: unmarshalDate(data.created_at),
-    id: data.id,
-    privateNetworkId: data.private_network_id,
-    projectId: data.project_id,
-    serverId: data.server_id,
-    status: data.status,
-    updatedAt: unmarshalDate(data.updated_at),
-    vlan: data.vlan,
-  } as ServerPrivateNetwork
-}
-
 export const unmarshalSetting = (data: unknown) => {
   if (!isJSONObject(data)) {
     throw new TypeError(
@@ -444,22 +420,6 @@ export const unmarshalListServerEventsResponse = (data: unknown) => {
   } as ListServerEventsResponse
 }
 
-export const unmarshalListServerPrivateNetworksResponse = (data: unknown) => {
-  if (!isJSONObject(data)) {
-    throw new TypeError(
-      `Unmarshalling the type 'ListServerPrivateNetworksResponse' failed as data isn't a dictionary.`,
-    )
-  }
-
-  return {
-    serverPrivateNetworks: unmarshalArrayOfObject(
-      data.server_private_networks,
-      unmarshalServerPrivateNetwork,
-    ),
-    totalCount: data.total_count,
-  } as ListServerPrivateNetworksResponse
-}
-
 export const unmarshalListServersResponse = (data: unknown) => {
   if (!isJSONObject(data)) {
     throw new TypeError(
@@ -484,21 +444,6 @@ export const unmarshalListSettingsResponse = (data: unknown) => {
     settings: unmarshalArrayOfObject(data.settings, unmarshalSetting),
     totalCount: data.total_count,
   } as ListSettingsResponse
-}
-
-export const unmarshalSetServerPrivateNetworksResponse = (data: unknown) => {
-  if (!isJSONObject(data)) {
-    throw new TypeError(
-      `Unmarshalling the type 'SetServerPrivateNetworksResponse' failed as data isn't a dictionary.`,
-    )
-  }
-
-  return {
-    serverPrivateNetworks: unmarshalArrayOfObject(
-      data.server_private_networks,
-      unmarshalServerPrivateNetwork,
-    ),
-  } as SetServerPrivateNetworksResponse
 }
 
 const marshalCreateServerRequestInstall = (
@@ -558,20 +503,6 @@ export const marshalInstallServerRequest = (
   service_user: request.serviceUser,
   ssh_key_ids: request.sshKeyIds,
   user: request.user,
-})
-
-export const marshalPrivateNetworkApiAddServerPrivateNetworkRequest = (
-  request: PrivateNetworkApiAddServerPrivateNetworkRequest,
-  defaults: DefaultValues,
-): Record<string, unknown> => ({
-  private_network_id: request.privateNetworkId,
-})
-
-export const marshalPrivateNetworkApiSetServerPrivateNetworksRequest = (
-  request: PrivateNetworkApiSetServerPrivateNetworksRequest,
-  defaults: DefaultValues,
-): Record<string, unknown> => ({
-  private_network_ids: request.privateNetworkIds,
 })
 
 export const marshalRebootServerRequest = (
