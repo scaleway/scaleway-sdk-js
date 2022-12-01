3e37f966b3d2a5c6256bd7b5c0223bd3800b4287
Vincent Germain <vgermain@scaleway.com>
2022-12-01 17:40:50 +0100

    feat(baremetal): add PrivateNetwork API

--- packages/clients/src/api/baremetal/v1/index.ts
+++ packages/clients/src/api/baremetal/v1/index.ts
@@ -1,3 +1,4 @@
 export { BaremetalV1UtilsAPI as API } from './api.utils'
+export { BaremetalPrivateNetworkV1GenAPI as PrivateNetworkAPI } from './api.gen'
 export * from './content.gen'
 export * from './types.gen'
