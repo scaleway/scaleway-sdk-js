3df0af7f9afd7f73279e46b2121e9622ec176bea
Vincent Germain <vgermain@scaleway.com>
2023-01-10 11:17:25 +0100

    chore: remove extra line

--- packages/clients/src/api/baremetal/v1/index.gen.ts
+++ packages/clients/src/api/baremetal/v1/index.gen.ts
@@ -1,6 +1,5 @@
 // This file was automatically generated. DO NOT EDIT.
 // If you have any remark or suggestion do not hesitate to open an issue.
-
 export { API, PrivateNetworkAPI } from './api.gen'
 export * from './content.gen'
 export type {
--- packages/clients/src/api/iam/v1alpha1/index.gen.ts
+++ packages/clients/src/api/iam/v1alpha1/index.gen.ts
@@ -1,6 +1,5 @@
 // This file was automatically generated. DO NOT EDIT.
 // If you have any remark or suggestion do not hesitate to open an issue.
-
 export { API } from './api.gen'
 export type {
   APIKey,
