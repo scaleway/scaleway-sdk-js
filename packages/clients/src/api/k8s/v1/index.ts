6ab9df88e4c4cc5119230c8111b17c43daed975c
Vincent Germain <vgermain@scaleway.com>
2023-03-01 17:30:48 +0100

    fix(k8s): export getClusterKubeConfig (#487)

---
+++ packages/clients/src/api/k8s/v1/index.ts
@@ -0,0 +1,5 @@
+export { K8SUtilsAPI as API } from './api.utils'
+export * from './content.gen'
+export * from './types.gen'
+export * from './types.utils'
+export * as ValidationRules from './validation-rules.gen'
