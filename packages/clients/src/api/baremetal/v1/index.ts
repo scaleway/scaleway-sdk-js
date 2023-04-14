9837e460e282d130caff68fd27446f05a541f1f9
Vincent Germain <vgermain@scaleway.com>
2023-04-14 13:42:15 +0200

    chore(readme): make the CORS information less scary (#607)

--- packages/clients/src/api/baremetal/v1/index.ts
+++ packages/clients/src/api/baremetal/v1/index.ts
@@ -1,5 +1,5 @@
 export { BaremetalV1UtilsAPI as API } from './api.utils'
 export { PrivateNetworkAPI } from './api.gen'
 export * from './content.gen'
-export * from './types.gen'
+export type * from './types.gen'
 export * as ValidationRules from './validation-rules.gen'
