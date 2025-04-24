b1733aaea997918b3745b9de895601b060f62ecf
protobuf-ci-cd <aphilibeaux@scaleway.com>
2025-04-24 14:07:15 +0200

    chore!: remove deprecated alias of multiples namespace
    BREAKING CHANGE: VPC, VPCGW, TransactionalEmail, ServerlessSQLDB, RDB, MNQ, LB, IPAM, IAM, FlexibleIP, BareMetal, AppleSilicon
    
    Signed-off-by: protobuf-ci-cd <protobuf-ci-cd@scaleway.com>

--- packages/clients/src/api/ipam/v1/validation-rules.gen.ts
+++
@@ -1,9 +0,0 @@
-// This file was automatically generated. DO NOT EDIT.
-// If you have any remark or suggestion do not hesitate to open an issue.
-
-export const ListIPsRequest = {
-  resourceName: {
-    maxLength: 63,
-    minLength: 2,
-  },
-}
--- packages/clients/src/api/lb/v1/index.ts
+++
@@ -1,4 +0,0 @@
-export { LbV1UtilsAPI as API, LbZonedV1UtilsAPI as ZonedAPI } from './api.utils'
-export * from './content.gen'
-export type * from './types.gen'
-export type * from './types.utils'
