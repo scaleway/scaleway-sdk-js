e985f34ccf34aead147ce99f6af5fea4dde6f158
Jonathan R. <jremy@scaleway.com>
2025-06-04 15:05:18 +0200

    feat(sdk-js): add new locality type (#2157)
    Co-authored-by: protobuf-ci-cd <protobuf-ci-cd@scaleway.com>
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

--- packages/client/src/scw/locality.ts
+++ packages/client/src/scw/locality.ts
@@ -13,3 +13,27 @@ export type Zone =
   | 'pl-waw-3'
   | (string & {})
 /* eslint-enable @typescript-eslint/ban-types */
+
+export type ApiLocality =
+  | { type: 'zone';       zones: Zone[] }
+  | { type: 'region';     regions: Region[] }
+  | { type: 'global' }
+  | { type: 'unspecified' }
+
+
+export function toApiLocality(
+  legacy?: { zones?: Zone[]; regions?: Region[] }
+): ApiLocality {
+  if (!legacy) {
+    return { type: 'unspecified' }
+  }
+  const { zones, regions } = legacy
+  if (zones && zones.length > 0) {
+    return { type: 'zone', zones }
+  }
+  if (regions && regions.length > 0) {
+    return { type: 'region', regions }
+  }
+  
+  return { type: 'global' }
+}
