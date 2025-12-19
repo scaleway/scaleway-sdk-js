428dc54465097659ca00ec211cdd48d5c5c5a908
Jonathan Remy <jremy@scaleway.com>
2025-12-19 11:57:58 +0100

    fix: format all generated config files

--- packages_generated/marketplace/vite.config.ts
+++ packages_generated/marketplace/vite.config.ts
@@ -6,9 +6,9 @@ import { defineConfig, mergeConfig } from 'vite'
 import { defaultConfig } from '../../vite.config'
 
 export default mergeConfig(defineConfig(defaultConfig), {
-	build: {
-		lib: {
-			entry: 'src/index.gen.ts',
-		},
-	},
+  build: {
+    lib: {
+      entry: 'src/index.gen.ts',
+    },
+  },
 })
