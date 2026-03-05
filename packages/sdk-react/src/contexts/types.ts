99e33a9347615232a8d6ae0c2fc2114cc8508473
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2026-03-05 15:44:00 +0100

    fix(sdk-react): expose factory
    Signed-off-by: Alexandre Philibeaux <aphilibeaux@scaleway.com>

--- packages/sdk-react/src/contexts/types.ts
+++ packages/sdk-react/src/contexts/types.ts
@@ -1,3 +1,14 @@
+import type { API } from '@scaleway/sdk-client'
 import type { APISdk } from '../types.generated'
 
-export type APISdkCache = APISdk
+export type DefaultTypeBaseAPI = Record<string, API>
+/**
+ * Base API SDK Cache type that includes all standard SDKs
+ */
+export type BaseAPISdkCache = APISdk
+
+export type ExtendedAPISdkCache<
+  TCustomAPIs extends DefaultTypeBaseAPI = Record<string, never>,
+> = BaseAPISdkCache & TCustomAPIs
+
+export type APISdkCache = ExtendedAPISdkCache<DefaultTypeBaseAPI>
--- tools/generate-react-sdk/src/generateAPI/config.ts
+++ tools/generate-react-sdk/src/generateAPI/config.ts
@@ -1,5 +1,3 @@
-import { readFileSync } from 'node:fs'
-import { resolve } from 'node:path'
 import { Project } from 'ts-morph'
 
 // Load the entry file
@@ -7,29 +5,3 @@ export const project = new Project()
 
 // We dont want these types to be suffixed by namespaces
 export const rawStringTypes = ['string', 'Blob', 'string[]']
-
-// Read package.json
-const packageJson = JSON.parse(
-  readFileSync(resolve(resolve('./'), 'package.json'), 'utf8'),
-) as {
-  dependencies?: Record<string, string>
-  devDependencies?: Record<string, string>
-  peerDependencies?: Record<string, string>
-}
-
-// Generate SDK paths from dependencies
-export const sdkPaths = [
-  ...Object.keys(packageJson.dependencies ?? {}),
-  ...Object.keys(packageJson.devDependencies ?? {}),
-  ...Object.keys(packageJson.peerDependencies ?? {}),
-]
-  .filter(dep => dep.startsWith('@scaleway/sdk'))
-  .filter(dep => !['@scaleway/sdk-client'].includes(dep))
-  .map(dep => {
-    const declarationFile = 'dist/index.gen.d.ts'
-
-    return [
-      dep,
-      resolve(resolve('./'), `./node_modules/${dep}/${declarationFile}`),
-    ]
-  })
