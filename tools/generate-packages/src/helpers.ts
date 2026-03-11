db1849a3634159c56aa2874f6e8065a98aeab774
Jonathan Remy <jremy@scaleway.com>
2026-03-11 15:52:29 +0100

    fix(generate-packages): restore acronym exceptions in snakeToPascal to avoid breaking change

--- tools/generate-packages/src/helpers.ts
+++ tools/generate-packages/src/helpers.ts
@@ -1,10 +1,20 @@
 const upperFirst = (str: string) =>
   str.slice(0, 1).toUpperCase() + str.slice(1, str.length)
 
+const ACRONYMS = new Set(['k8s', 's2s', 'api', 'http', 'https', 'url', 'uri'])
+
+const handleAcronym = (str: string): string => {
+  const lower = str.toLowerCase()
+  if (ACRONYMS.has(lower)) {
+    return str.toUpperCase()
+  }
+  return upperFirst(str)
+}
+
 export const snakeToPascal = (str: string) =>
   str
     .split('_')
-    .map(s => upperFirst(s.split('/').map(upperFirst).join('/')))
+    .map(s => handleAcronym(s.split('/').map(handleAcronym).join('/')))
     .join('')
 
 export const unionTocamelCase = (str: string) =>
@@ -15,7 +25,7 @@ export const snakeToSlug = (str: string) => str.split('_').join('-')
 export const snakeToDisplayName = (str: string) =>
   str
     .split('_')
-    .map(s => upperFirst(s.split('/').map(upperFirst).join('/')))
+    .map(s => handleAcronym(s.split('/').map(handleAcronym).join('/')))
     .join(' ')
 
 export const renderTemplate = (
