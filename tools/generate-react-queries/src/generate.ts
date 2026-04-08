0efc1275bc6e412cc20c01dec3c0c8ecd4c6499e
Manu Chambon <echambon@scaleway.com>
2026-04-08 15:48:22 +0200

    fix: convert deprecated rmDirSync recursive to rmSync (#2888)
    Signed-off-by: Emmanuel CHAMBON <echambon@scaleway.com>

--- tools/generate-react-queries/src/generate.ts
+++ tools/generate-react-queries/src/generate.ts
@@ -64,7 +64,7 @@ export async function generateFromMetadata(
         )
 
         if (existsSync(generatedDir)) {
-          rmSync(generatedDir, { recursive: true })
+          rmSync(generatedDir, { recursive: true, force: true })
         }
         mkdirSync(generatedDir, { recursive: true })
 
