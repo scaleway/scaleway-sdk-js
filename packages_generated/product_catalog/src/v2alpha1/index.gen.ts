9713b3345a733580ca63e3aab6d22c4e79fec53e
Jonathan Remy <jremy@scaleway.com>
2026-05-20 14:54:59 +0200

    feat(product_catalog): add PublicCatalogProductPropertiesServerlessFunctions

--- packages_generated/product_catalog/src/v2alpha1/index.gen.ts
+++ packages_generated/product_catalog/src/v2alpha1/index.gen.ts
@@ -61,6 +61,7 @@ export type {
   PublicCatalogProductPropertiesObjectStorageRestoreType,
   PublicCatalogProductPropertiesObjectStorageRestoreTypeRestoreType,
   PublicCatalogProductPropertiesSecretManager,
+  PublicCatalogProductPropertiesServerlessFunctions,
   PublicCatalogProductStatus,
   PublicCatalogProductUnitOfMeasure,
   PublicCatalogProductUnitOfMeasureCountableUnit,
