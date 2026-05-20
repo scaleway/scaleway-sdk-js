9287c2e50bc1c9906fa98c462f20c281a10f4963
Jonathan Remy <jremy@scaleway.com>
2026-05-20 14:52:42 +0200

    chore: reformat tsconfig.build.json exclude arrays and reorder imports

--- packages_generated/jobs/src/v1alpha2/api.gen.ts
+++ packages_generated/jobs/src/v1alpha2/api.gen.ts
@@ -1,23 +1,18 @@
 // This file was automatically generated. DO NOT EDIT.
 // If you have any remark or suggestion do not hesitate to open an issue.
-
-import type { ApiLocality,} from '@scaleway/sdk-client'
 import {
-  enrichForPagination,
   API as ParentAPI,
-  toApiLocality,
+  enrichForPagination,
   urlParams,
   validatePathParam,
+  toApiLocality,
 } from '@scaleway/sdk-client'
+import type { ApiLocality,} from '@scaleway/sdk-client'
 import {
   marshalCreateJobDefinitionRequest,
   marshalCreateSecretsRequest,
-  marshalCreateTriggerRequest,
-  marshalStartJobDefinitionRequest,
-  marshalUpdateJobDefinitionRequest,
-  marshalUpdateSecretRequest,
-  marshalUpdateTriggerRequest,
   unmarshalCreateSecretsResponse,
+  marshalCreateTriggerRequest,
   unmarshalJobDefinition,
   unmarshalJobLimits,
   unmarshalJobRun,
@@ -27,8 +22,12 @@ import {
   unmarshalListSecretsResponse,
   unmarshalListTriggersResponse,
   unmarshalSecret,
+  marshalStartJobDefinitionRequest,
   unmarshalStartJobDefinitionResponse,
   unmarshalTrigger,
+  marshalUpdateJobDefinitionRequest,
+  marshalUpdateSecretRequest,
+  marshalUpdateTriggerRequest,
 } from './marshalling.gen.js'
 import type {
   CreateJobDefinitionRequest,
