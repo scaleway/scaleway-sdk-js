294b04ca67bcf7e3ec42abf85bfe6324276ea665
Rémy Léone <rleone@scaleway.com>
2026-09-07 15:53:51 +0200

    Fix

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
     'typescript/no-unsafe-assignment': 'warn',
     'typescript/no-unsafe-return': 'warn',
     'typescript/consistent-type-definitions': 'warn',
-    'eslint/no-console': 'warn',
     'eslint/no-continue': 'warn',
     'eslint/no-unused-vars': 'warn',
     'eslint/max-statements': 'warn',
