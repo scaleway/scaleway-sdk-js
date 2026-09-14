4e0708f3913a90f1a8a79cc059f4bc640bc5a604
Rémy Léone <rleone@scaleway.com>
2026-09-14 14:51:14 +0200

    Fix

--- tools/generate-packages/templates/vite.config.ts
+++ tools/generate-packages/templates/vite.config.ts
@@ -1,7 +1,8 @@
 import { viteConfig } from '@repo/configs'
 import { defineConfig, mergeConfig } from 'vite'
+import type { UserConfig } from 'vite'
 
-export default mergeConfig(defineConfig(viteConfig), {
+export default mergeConfig(defineConfig(viteConfig as UserConfig), {
   build: {
     lib: {
       entry: ['src/index.gen.ts', 'src/metadata.gen.ts'],
